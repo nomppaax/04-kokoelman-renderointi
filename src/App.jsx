a97d4a7772306b0a830787a0b513b779ec892c24
Noomi <kykkanenn@gmail.com>
2024-02-26 15:20:18 +0200

    12-Alkuarvo on 10

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,17 @@ import viteLogo from "/vite.svg";
 const App = (props) => {
   const [value, setValue] = useState(10);
 
-  const setToValue = (newValue) => () => {
-    console.log("value now", newValue); // tulostetaan uusi arvo konsoliin
+  const setToValue = (newValue) => {
+    console.log("value now", newValue);
     setValue(newValue);
   };
 
   return (
     <div>
       {value}
-      <button onClick={setToValue(1000)}>thousand</button>
-      <button onClick={setToValue(0)}>reset</button>
-      <button onClick={setToValue(value + 1)}>increment</button>
+      <button onClick={() => setToValue(1000)}>thousand</button>
+      <button onClick={() => setToValue(0)}>reset</button>
+      <button onClick={() => setToValue(value + 1)}>increment</button>
     </div>
   );
 };
