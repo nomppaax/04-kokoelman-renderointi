1888114e424684bf0a9c2af6631b95b20d37ff05
Noomi <kykkanenn@gmail.com>
2024-02-26 14:33:09 +0200

    07-Painalluksien historia luotu

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return <div>the app is used by pressing the buttons</div>;
+  }
+  return <div>button press history: {props.allClicks.join(" ")}</div>;
+};
 
 const App = () => {
   const [left, setLeft] = useState(0);
@@ -25,10 +31,13 @@ const App = () => {
 
   return (
     <div>
-      {left}
-      <button onClick={handleLeftClick}>left</button>
-      <button onClick={handleRightClick}>right</button>
-      {right}
+      <div>
+        {left}
+        <button onClick={handleLeftClick}>left</button>
+        <button onClick={handleRightClick}>right</button>
+        {right}
+        <History allClicks={allClicks} />
+      </div>
       <p>{allClicks.join(" ")}</p>
 
       <p>total {total}</p>
