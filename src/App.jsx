92fc57b49c97a8663ed38108eb561686268fe0f8
Noomi <kykkanenn@gmail.com>
2024-02-26 15:17:22 +0200

    10-Kolme nappia (world, react, function)

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,20 @@ import viteLogo from "/vite.svg";
 const App = (props) => {
   const [value, setValue] = useState(10);
 
-  const hello = () => {
-    const handler = () => console.log("hello world");
+  const hello = (who) => {
+    const handler = () => {
+      console.log("hello", who);
+    };
     return handler;
   };
 
   return (
     <div>
       {value}
-      <button onClick={hello()}>button</button>
+
+      <button onClick={hello("world")}>button</button>
+      <button onClick={hello("react")}>button</button>
+      <button onClick={hello("function")}>button</button>
     </div>
   );
 };
