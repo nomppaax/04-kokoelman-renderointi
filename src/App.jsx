296a96ed9db1a9112463a1e2f35b3f9fedbb4708
Noomi <kykkanenn@gmail.com>
2024-02-26 15:15:09 +0200

    09-Painallus nollaa ja konsoliin: Hello world

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,15 @@ import viteLogo from "/vite.svg";
 const App = (props) => {
   const [value, setValue] = useState(10);
 
-  const handleClick = () => {
-    console.log("clicked the button");
-    setValue(0);
+  const hello = () => {
+    const handler = () => console.log("hello world");
+    return handler;
   };
 
   return (
     <div>
       {value}
-      <button onClick={handleClick}>button</button>
+      <button onClick={hello()}>button</button>
     </div>
   );
 };
