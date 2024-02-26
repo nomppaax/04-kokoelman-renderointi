7e7de7f2405ab81eac5df38c86066398a7953fe2
Noomi <kykkanenn@gmail.com>
2024-02-26 14:21:39 +0200

    04-allClicks muistaa kaikki painallukset ja tulostaa L tai R

--- src/App.jsx
+++ src/App.jsx
@@ -3,34 +3,30 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 
 const App = () => {
-  const [clicks, setClicks] = useState({
-    left: 0,
-    right: 0,
-  });
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
+
+  const [allClicks, setAll] = useState([]);
 
   const handleLeftClick = () => {
-    const newClicks = {
-      left: clicks.left + 1,
-      right: clicks.right,
-    };
-    setClicks(newClicks);
+    setAll(allClicks.concat("L"));
+    setLeft(left + 1);
   };
 
   const handleRightClick = () => {
-    const newClicks = {
-      left: clicks.left,
-      right: clicks.right + 1,
-    };
-    setClicks(newClicks);
+    setAll(allClicks.concat("R"));
+    setRight(right + 1);
   };
 
   return (
     <div>
       <div>
-        {clicks.left}
+        {left}
         <button onClick={handleLeftClick}>left</button>
         <button onClick={handleRightClick}>right</button>
-        {clicks.right}
+        {right}
+
+        <p>{allClicks.join(" ")}</p>
       </div>
     </div>
   );
