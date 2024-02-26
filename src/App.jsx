4b95738112e124e17c11e086e974de407aeb5a7f
Noomi <kykkanenn@gmail.com>
2024-02-26 14:15:28 +0200

    02-Kaksi alkuarvon 0 saavaa tilaa left ja right ja napit

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,19 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 
-function App() {
-  const [count, setCount] = useState(0);
+const App = () => {
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
 
   return (
-    <>
+    <div>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {left}
+        <button onClick={() => setLeft(left + 1)}>left</button>
+        <button onClick={() => setRight(right + 1)}>right</button>
+        {right}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   );
-}
-
+};
 export default App;
