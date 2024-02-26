39b9a4be77b81d50fc5bbc0c61bb3b0ddfb465d3
Noomi <kykkanenn@gmail.com>
2024-02-26 14:25:42 +0200

    05-Total näyttää kaikki painallukset

--- src/App.jsx
+++ src/App.jsx
@@ -5,29 +5,33 @@ import viteLogo from "/vite.svg";
 const App = () => {
   const [left, setLeft] = useState(0);
   const [right, setRight] = useState(0);
-
   const [allClicks, setAll] = useState([]);
+
+  const [total, setTotal] = useState(0);
 
   const handleLeftClick = () => {
     setAll(allClicks.concat("L"));
     setLeft(left + 1);
+
+    setTotal(left + right);
   };
 
   const handleRightClick = () => {
     setAll(allClicks.concat("R"));
     setRight(right + 1);
+
+    setTotal(left + right);
   };
 
   return (
     <div>
-      <div>
-        {left}
-        <button onClick={handleLeftClick}>left</button>
-        <button onClick={handleRightClick}>right</button>
-        {right}
+      {left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {right}
+      <p>{allClicks.join(" ")}</p>
 
-        <p>{allClicks.join(" ")}</p>
-      </div>
+      <p>total {total}</p>
     </div>
   );
 };
