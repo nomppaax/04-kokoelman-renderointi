b5daf2704b93c4ee9d60928aaf77e72f4fe1bc8e
Noomi <kykkanenn@gmail.com>
2024-02-26 15:18:37 +0200

    11-Kolme nappia(lisää tuhat, nollaa, lisää 1)

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,17 @@ import viteLogo from "/vite.svg";
 const App = (props) => {
   const [value, setValue] = useState(10);
 
-  const hello = (who) => {
-    const handler = () => {
-      console.log("hello", who);
-    };
-    return handler;
+  const setToValue = (newValue) => () => {
+    console.log("value now", newValue); // tulostetaan uusi arvo konsoliin
+    setValue(newValue);
   };
 
   return (
     <div>
       {value}
-
-      <button onClick={hello("world")}>button</button>
-      <button onClick={hello("react")}>button</button>
-      <button onClick={hello("function")}>button</button>
+      <button onClick={setToValue(1000)}>thousand</button>
+      <button onClick={setToValue(0)}>reset</button>
+      <button onClick={setToValue(value + 1)}>increment</button>
     </div>
   );
 };
