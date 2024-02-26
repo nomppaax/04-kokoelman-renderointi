592175dc26bf9a507bb693bf869d52a7a0a993ee
Noomi <kykkanenn@gmail.com>
2024-02-26 15:13:13 +0200

    08-Tapahtumankäsittely revisited

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,19 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-const History = (props) => {
-  if (props.allClicks.length === 0) {
-    return <div>the app is used by pressing the buttons</div>;
-  }
-  return <div>button press history: {props.allClicks.join(" ")}</div>;
-};
 
-const App = () => {
-  const [left, setLeft] = useState(0);
-  const [right, setRight] = useState(0);
-  const [allClicks, setAll] = useState([]);
+const App = (props) => {
+  const [value, setValue] = useState(10);
 
-  const [total, setTotal] = useState(0);
-
-  const handleLeftClick = () => {
-    setAll(allClicks.concat("L"));
-    const updatedLeft = left + 1;
-    setLeft(updatedLeft);
-    setTotal(updatedLeft + right);
-  };
-
-  const handleRightClick = () => {
-    setAll(allClicks.concat("R"));
-    const updatedRight = right + 1;
-    setRight(updatedRight);
-    setTotal(updatedRight + left);
+  const handleClick = () => {
+    console.log("clicked the button");
+    setValue(0);
   };
 
   return (
     <div>
-      <div>
-        {left}
-        <button onClick={handleLeftClick}>left</button>
-        <button onClick={handleRightClick}>right</button>
-        {right}
-        <History allClicks={allClicks} />
-      </div>
-      <p>{allClicks.join(" ")}</p>
-
-      <p>total {total}</p>
+      {value}
+      <button onClick={handleClick}>button</button>
     </div>
   );
 };
